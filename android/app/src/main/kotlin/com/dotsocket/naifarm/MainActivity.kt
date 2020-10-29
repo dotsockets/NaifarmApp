bfd374f70c1e219e01b3607525776b5680845b04
ApisitKaewsasan <ApisitKaewasasan@hotmail.com>
2020-10-29 11:47:45 +0700

    Create Naifarm App

---
+++ android/app/src/main/kotlin/com/dotsocket/naifarm/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.dotsocket.naifarm
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+//class MainActivity: FlutterActivity() {
+//}
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+//        FacebookSdk.setApplicationId(getString(R.string.facebook_app_id))
+//        FacebookSdk.sdkInitialize(getApplicationContext());
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+    }
+}
