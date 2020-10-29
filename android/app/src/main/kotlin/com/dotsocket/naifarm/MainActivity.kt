91c0a4e447a1c6e0b35a53e8a48edc5aeea9ecd9
ApisitKaewsasan <ApisitKaewasasan@hotmail.com>
2020-10-29 14:22:25 +0700

    build --flavor. development , staging , production

--- android/app/src/main/kotlin/com/dotsocket/naifarm/MainActivity.kt
+++ android/app/src/main/kotlin/com/dotsocket/naifarm/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.dotsocket.naifarm
 
+import com.facebook.FacebookSdk
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
@@ -9,8 +10,8 @@ import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-//        FacebookSdk.setApplicationId(getString(R.string.facebook_app_id))
-//        FacebookSdk.sdkInitialize(getApplicationContext());
+        FacebookSdk.setApplicationId(getString(R.string.facebook_app_id))
+        FacebookSdk.sdkInitialize(getApplicationContext());
         GeneratedPluginRegistrant.registerWith(flutterEngine)
     }
 }
