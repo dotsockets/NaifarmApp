0764b8cff5c734a54c726794f46e5bd2f87ab9ac
ApisitKaewsasan <ApisitKaewasasan@hotmail.com>
2021-07-07 19:54:04 +0700

    no message

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -4,7 +4,6 @@
 
 // ignore_for_file: lines_longer_than_80_chars
 
-import 'package:device_info_plus_web/device_info_plus_web.dart';
 import 'package:image_picker_for_web/image_picker_for_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
 import 'package:uni_links_web/uni_links_web.dart';
@@ -16,7 +15,6 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
 void registerPlugins(Registrar registrar) {
-  DeviceInfoPlusPlugin.registerWith(registrar);
   ImagePickerPlugin.registerWith(registrar);
   SharedPreferencesPlugin.registerWith(registrar);
   UniLinksPlugin.registerWith(registrar);
