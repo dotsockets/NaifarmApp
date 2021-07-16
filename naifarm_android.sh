56dec9766b4c614e63d06055751b2e630df7a720
ApisitKaewsasan <ApisitKaewasasan@hotmail.com>
2021-07-16 09:26:00 +0700

    build android ios 2.1.1

--- naifarm_android.sh
+++ naifarm_android.sh
@@ -1,7 +1,7 @@
 #!/bin/bash appbundle
 flutter clean
 flutter pub get
-flutter build appbundle --target-platform android-arm,android-arm64  --build-name=1.0.4 --build-number=39 --flavor production  -t lib/config/Main_production.dart
+flutter build appbundle --target-platform android-arm,android-arm64  --build-name=2.1.1 --build-number=44 --flavor production  -t lib/config/Main_production.dart
 cd android
 fastlane distribute_prod
 
